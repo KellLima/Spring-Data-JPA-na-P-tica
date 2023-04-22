b77111b38205794bd627a0d66fff820e5a5d43aa
Kell <kennelrak@gmail.com>
2023-04-22 19:24:41 -0300

    SpringDataJPA

---
+++ src/main/java/me/dio/academia/digital/entity/Matricula.java
@@ -0,0 +1,28 @@
+package me.dio.academia.digital.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_matriculas")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handle"})
+public class Matricula {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
+  private Aluno aluno;
+
+  private LocalDateTime dataDaMatricula = LocalDateTime.now();
+}
